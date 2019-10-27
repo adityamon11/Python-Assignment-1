ff2ea960eb7a51dab2c24c1ce6daedab0cb5cc6c
adityamon11 <adityakulkarni270@gmail.com>
2019-10-27 12:43:15 +0530

    Add files via upload

---
+++ Assignment1_3.py
@@ -0,0 +1,10 @@
+def Add(num1,num2):
+	ret = num1+num2;
+	return ret;
+
+print("Enter first number : ");
+x = int(input());
+print("Enter second number : ");
+y = int(input());
+iret = Add(x,y);
+print(iret);
